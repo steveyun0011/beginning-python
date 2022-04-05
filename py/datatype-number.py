0678924a99b493212d7426764e928e743446d7ee
steveyun0011 <102004457+steveyun0011@users.noreply.github.com>
2022-04-05 17:17:19 +0900

    Add files via upload

---
+++ py/datatype-number.py
@@ -0,0 +1,21 @@
+from tkinter import E
+
+
+a=1
+print(type(a))
+#정수=int=integer 인티절
+
+b=1.123
+c=1.23e4
+c=1.23*10000
+print(c)
+print(type(b))
+print(type(c))
+#float=실수=real number 유리수=rational number
+
+d=3
+e=2
+print(d**e)
+print(d/e)
+print(d%e)
+#제곱은 ** 나누기의 몫(=정수)은 % java에서는 //
